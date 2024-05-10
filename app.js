aa1b6985ddd4cd2aa32c54dd79459d7f300926ec
sooraj <soorajnambiar8@gmail.com>
2024-05-10 20:38:38 +0530

    added form submission

--- app.js
+++ app.js
@@ -14,28 +14,30 @@ app.get("/", (req, res) => {
 });
 
 app.get("/home", (req, res) => {
-  res.send("index")
-})
+  res.render("home");
+});
 
-// app.get("/home", (req, res) => {
-//   const apiUrl = `https://api.waifu.pics/sfw/waify`;
-//   // const apiUrl = `https://api.waifu.pics/${type}/${categoru}`;
+app.post("/home", (req, res) => {
+  const category = req.body.category;
+  const apiUrl = `https://api.waifu.pics/sfw/${category}`;
+  // const apiUrl = `https://api.waifu.pics/${type}/${categoru}`;
 
-//   fetch(apiUrl)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok");
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       const imageUrl = data.url;
-//       res.render("index.ejs", { imageUrl });
-//     })
-//     .catch((error) => {
-//       console.error("There was a problem with the fetch operation:", error);
-//     });
-// });
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const imageUrl = data.url;
+      res.render("index.ejs", { imageUrl });
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+  // res.send("code");
+});
 
 app.listen(3000, () => {
   console.log("Connection successful");
